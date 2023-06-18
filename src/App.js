94147e9260fabdf36c4dde39f4604226e5dcb193
Joshua Sinkinson <joshuasinkinson93@gmail.com>
2023-06-18 21:39:00 +0100

    Set up initial folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/register" element={<Register />}  />
+            <Route path="/" element={<Home />}  />
+            <Route path="home" element={<Navigate to="/" />} />
+            <Route path="set" element={<Set />}  />
+            <Route path="profile/:userId" element={<Profile />}  />
+            <Route path="profile/:userId/sets" element={<Sets />}  />
+            <Route path="profile/:userId/sets/:setId" element={<Set />}  />
+            <Route path="profile/:userId/sounds" element={<Sounds />}  />
+            <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
