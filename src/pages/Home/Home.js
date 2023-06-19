ea412bcbc0f0f69864d03a07f771da33f9e852c3
Joshua Sinkinson <joshuasinkinson93@gmail.com>
2023-06-19 14:41:36 +0100

    Set up initial styling for genre cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,29 @@
 import "./Home.scss";
 
 const Home = () => {
-  return <></>;
+  return (
+    <main className="home">
+      <h1 className="home__title">Select Genre</h1>
+      <div className="home__genres-container">
+        <article className="genre">
+          <h2 className="genre__title">Raw Deep</h2>
+          <h3 className="genre__subtitle">House</h3>
+        </article>
+        <article className="genre">
+          <h2 className="genre__title">Raw Deep</h2>
+          <h3 className="genre__subtitle">House</h3>
+        </article>
+        <article className="genre">
+          <h2 className="genre__title">Raw Deep</h2>
+          <h3 className="genre__subtitle">House</h3>
+        </article>
+        <article className="genre">
+          <h2 className="genre__title">Raw Deep</h2>
+          <h3 className="genre__subtitle">House</h3>
+        </article>
+      </div>
+    </main>
+  )
 };
 
 export default Home;
