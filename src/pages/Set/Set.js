94d765f1cfb026e56a83f3e45d973f70f659057d
Joshua Sinkinson <joshuasinkinson93@gmail.com>
2023-06-21 13:51:56 +0100

    Refactor initial sounds async function

--- src/pages/Set/Set.js
+++ src/pages/Set/Set.js
@@ -20,6 +20,86 @@ const Set = () => {
     return Math.floor(Math.random() * arr.length)
   }
 
+  const getBass = async (subgenre, key_scale, rel_key_scale) => {
+    try {
+      const bassQuery = `?type=bass&subgenre=${subgenre}&key_scale=${key_scale}&rel_key_scale=${rel_key_scale}`;
+      const bassQueryRel = `?type=bass&subgenre=${subgenre}&key_scale=${rel_key_scale}&rel_key_scale=${key_scale}`;
+      const bassQueryAny = `?type=bass&subgenre=${subgenre}`;
+
+      // Request bass in the same key & scale as the chords
+      const bassResponse = await axios.get(
+        `${process.env.REACT_APP_API_URL}/sounds${bassQuery}`
+      );
+
+        // If no match is found, request bass in the same relative key & scale as the chords
+        if (bassResponse.data.length === 0) {
+          const bassResponseRel = await axios.get(
+            `${process.env.REACT_APP_API_URL}/sounds${bassQueryRel}`
+          );
+    
+          setBass(bassResponseRel.data[randomIndex(bassResponseRel.data)])
+
+          // If no match is found again, request bass in any key & scale
+          if (bassResponseRel.data.length === 0) {
+            const bassResponseAny = await axios.get(
+              `${process.env.REACT_APP_API_URL}/sounds${bassQueryAny}`
+            );
+
+            setBass(bassResponseAny.data[randomIndex(bassResponseAny.data)])
+          
+            return;
+          }
+
+        setBass(bassResponse.data[randomIndex(bassResponse.data)])
+      }
+      
+    } catch (error) {
+      console.log(
+        `Error ${error.response.status}: ${error.response.data.message}`
+      );
+    }
+  }
+
+  const getSynth = async (subgenre, key_scale, rel_key_scale) => {
+    try {
+      const synthQuery = `?type=synth&subgenre=${subgenre}&key_scale=${key_scale}&rel_key_scale=${rel_key_scale}`;
+      const synthQueryRel = `?type=synth&subgenre=${subgenre}&key_scale=${rel_key_scale}&rel_key_scale=${key_scale}`;
+      const synthQueryAny = `?type=synth&subgenre=${subgenre}`;
+
+      // Request synth in the same key & scale as the chords
+      const synthResponse = await axios.get(
+        `${process.env.REACT_APP_API_URL}/sounds${synthQuery}`
+      );
+
+        // If no match is found, request synth in the same relative key & scale as the chords
+        if (synthResponse.data.length === 0) {
+          const synthResponseRel = await axios.get(
+            `${process.env.REACT_APP_API_URL}/sounds${synthQueryRel}`
+          );
+    
+          setSynth(synthResponseRel.data[randomIndex(synthResponseRel.data)])
+
+          // If no match is found again, request synth in any key & scale
+          if (synthResponseRel.data.length === 0) {
+            const synthResponseAny = await axios.get(
+              `${process.env.REACT_APP_API_URL}/sounds${synthQueryAny}`
+            );
+
+            setSynth(synthResponseAny.data[randomIndex(synthResponseAny.data)])
+          
+            return;
+          }
+
+        setSynth(synthResponse.data[randomIndex(synthResponse.data)])
+      }
+      
+    } catch (error) {
+      console.log(
+        `Error ${error.response.status}: ${error.response.data.message}`
+      );
+    }
+  }
+
   const getInitialSounds = async () => {
     try {
 
@@ -44,68 +124,10 @@ const Set = () => {
       setHarmony(harmonyResponse.data[randomIndex(harmonyResponse.data)])
 
       // BASS
-      const bassQuery = `?type=bass&subgenre=${subgenre}&key_scale=${key_scale}&rel_key_scale=${rel_key_scale}`;
-      const bassQueryRel = `?type=bass&subgenre=${subgenre}&key_scale=${rel_key_scale}&rel_key_scale=${key_scale}`;
-      const bassQueryAny = `?type=bass&subgenre=${subgenre}`;
-
-      // Request bass in the same key & scale as the chords
-      const bassResponse = await axios.get(
-        `${process.env.REACT_APP_API_URL}/sounds${bassQuery}`
-      );
-
-      setBass(bassResponse.data[randomIndex(bassResponse.data)])
-
-      let bassResponseRel = []
-
-      // If no match is found, request bass in the same relative key & scale as the chords
-      if (bassResponse.data.length === 0) {
-        bassResponseRel = await axios.get(
-          `${process.env.REACT_APP_API_URL}/sounds${bassQueryRel}`
-        );
-
-        setBass(bassResponseRel.data[randomIndex(bassResponseRel.data)])
-      }
-
-      // If no match is found again, request bass in any key & scale
-      if (bassResponseRel.data.length === 0) {
-        const bassResponseAny = await axios.get(
-          `${process.env.REACT_APP_API_URL}/sounds${bassQueryAny}`
-        );
-
-        setBass(bassResponseAny.data[randomIndex(bassResponseAny.data)])
-      }
+      getBass(subgenre, key_scale, rel_key_scale);
 
       // SYNTH
-      const synthQuery = `?type=synth&subgenre=${subgenre}&key_scale=${key_scale}&rel_key_scale=${rel_key_scale}`;
-      const synthQueryRel = `?type=synth&subgenre=${subgenre}&key_scale=${rel_key_scale}&rel_key_scale=${key_scale}`;
-      const synthQueryAny = `?type=synth&subgenre=${subgenre}`;
-
-      // Request synth in the same key & scale as the chords
-      const synthResponse = await axios.get(
-        `${process.env.REACT_APP_API_URL}/sounds${synthQuery}`
-      );
-
-      setSynth(synthResponse.data[randomIndex(synthResponse.data)])
-
-      let synthResponseRel = []
-
-      // If no match is found, request synth in the same relative key & scale as the chords
-      if (synthResponse.data.length === 0) {
-        synthResponseRel = await axios.get(
-          `${process.env.REACT_APP_API_URL}/sounds${synthQueryRel}`
-        );
-
-        setSynth(synthResponseRel.data[randomIndex(synthResponseRel.data)])
-      }
-
-      // If no match is found again, request synth in any key & scale
-      if (synthResponseRel.data.length === 0) {
-        const synthResponseAny = await axios.get(
-          `${process.env.REACT_APP_API_URL}/sounds${synthQueryAny}`
-        );
-
-        setSynth(synthResponseAny.data[randomIndex(synthResponseAny.data)])
-      }
+      getSynth(subgenre, key_scale, rel_key_scale);
 
     } catch (error) {
       console.log(
